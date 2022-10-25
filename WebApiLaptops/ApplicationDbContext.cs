a953260cc992dd9e433d69ed39b064134552be21
Alan <alan_darkstar@hotmail.com>
2022-10-24 23:41:40 -0500

    Se agrego la conexion a la base de datos

---
+++ WebApiLaptops/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using WebApiLaptops.Entidades;
+
+namespace WebApiLaptops
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {        
+
+        }
+
+        public DbSet<Laptop> Laptops { get; set; }
+
+    }
+}
