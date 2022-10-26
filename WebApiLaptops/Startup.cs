855556288fa09a6f0289b02012e3b67c2ff70963
Alan <alan_darkstar@hotmail.com>
2022-10-25 21:04:42 -0500

    Se creo la configuracion de la base de datos

--- WebApiLaptops/Startup.cs
+++ WebApiLaptops/Startup.cs
@@ -1,4 +1,8 @@
-﻿namespace WebApiLaptops
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace WebApiLaptops
 {
     public class Startup
     {
@@ -13,8 +17,13 @@
         {
             services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiLaptops", Version = "v1" });
+            });
 
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
